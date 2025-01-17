125d822b267170ff359597275a7fbcd6a7db2088
Gabriel Marcone <gabrielmarconems@gmail.com>
2025-01-17 14:28:17 -0300

    adição dos pinos de controle dos leds e do buzzer

--- Tarefa2.c
+++ Tarefa2.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Definições dos LEDs RGB
+#define LED_RED 13
+#define LED_BLUE 12
+#define LED_GREEN 11
+
+// Definição do buzzer
+#define BUZZER 21
 
 
 int main()
